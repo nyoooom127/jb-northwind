0faa6513107bf17d5f48c548ea946be08090f40d
nyoooom <tamar.peles@gmail.com>
2023-09-03 13:22:06 +0300

    header

--- src/Components/LayoutArea/Layout/Layout.tsx
+++ src/Components/LayoutArea/Layout/Layout.tsx
@@ -1,9 +1,10 @@
+import Header from "../Header/Header";
 import "./Layout.css";
 
 function Layout(): JSX.Element {
     return (
         <div className="Layout">
-			<header>1</header>
+            <header><Header /></header>
             <aside>2</aside>
             <main>3</main>
             <footer>4</footer>
