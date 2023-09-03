c5cc2fb8416dcc92f8ef468f110a809ff4dde621
nyoooom <tamar.peles@gmail.com>
2023-09-03 13:10:52 +0300

    init

---
+++ src/Components/LayoutArea/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import "./Layout.css";
+
+function Layout(): JSX.Element {
+    return (
+        <div className="Layout">
+			<header>1</header>
+            <aside>2</aside>
+            <main>3</main>
+            <footer>4</footer>
+        </div>
+    );
+}
+
+export default Layout;
