22a90671076504f25c8c907fb92eb357ff67b8bc
nyoooom <tamar.peles@gmail.com>
2023-09-10 11:39:09 +0300

    mui

--- src/Components/AboutArea/ContactUs/ContactUs.tsx
+++ src/Components/AboutArea/ContactUs/ContactUs.tsx
@@ -1,3 +1,7 @@
+import ContactMailIcon from '@mui/icons-material/ContactMail';
+import ForwardToInboxIcon from '@mui/icons-material/ForwardToInbox';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { Button, ButtonGroup, Checkbox, FormControlLabel, TextField, Typography } from "@mui/material";
 import useTitle from "../../../Utils/UseTitle";
 import "./ContactUs.css";
 
@@ -6,7 +10,26 @@ function ContactUs(): JSX.Element {
 
     return (
         <div className="ContactUs">
-            <p>nope... We're on a coffee break</p>
+            <form>
+
+                <Typography variant='h3'>Contact Us &nbsp; <ContactMailIcon fontSize='large' /></Typography>
+
+                <TextField label="Name" variant="outlined" className="TextBox" />
+
+                <TextField label="Email" type="email" variant="outlined" className="TextBox" />
+
+                <TextField label="Phone" type="tel" variant="outlined" className="TextBox" />
+
+                <TextField label="Message" variant="outlined" className="TextBox" />
+
+                <FormControlLabel label="Send me promotional emails" control={<Checkbox />} className="Flex" />
+
+                <ButtonGroup variant="contained" fullWidth>
+                    <Button color="primary">Send &nbsp; <ForwardToInboxIcon /></Button>
+                    <Button type="reset" color="secondary">Clear &nbsp; <HighlightOffIcon /></Button>
+                </ButtonGroup>
+
+            </form>
         </div>
     );
 }
